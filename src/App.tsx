74526237ef2d28cf5fe17b78cc180d20d49269bb
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2025-05-09 00:33:39 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+function App() {
+
+  return (
+    <Typography variant="h4" component="h1" gutterBottom>
+      Test
+    </Typography>
+  )
+}
+
+export default App
